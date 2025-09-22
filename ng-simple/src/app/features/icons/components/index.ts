70d941c0184e092390796eea7bb35dff77c69950
Tiago Barracha <taigo.barracha@gmail.com>
2025-09-22 19:07:56 +0100

    refac(FE): removed ng simple

--- ng-simple/src/app/features/icons/components/index.ts
+++
@@ -1,5 +0,0 @@
-export { IconVisibilityComponent } from './icon-visibility.component';
-export { IconVisibilityOffComponent } from './icon-visibilityOff.component';
-export { IconSettingsComponent } from './icon-settings.component';
-export { IconAccountComponent } from './icon-account.component';
-export { IconLogoutComponent } from './icon-logout.component';
--- ng-simple/src/app/spire-lib/core/operations/streaming/stream-cancel-dtos.ts
+++
@@ -1,8 +0,0 @@
-// File: src/app/modules/streaming/stream-cancel-dtos.ts
-export interface CancelStreamRequestDto {
-  requestId: string;
-}
-
-export interface CancelStreamResponseDto {
-  success: boolean;
-}
